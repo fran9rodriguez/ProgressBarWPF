bcdf29c9b9934e84e3b8e6207c0f263abe076a01
Fran Rodriguez Montero <fran.rodriguez@tiekinetix.com>
2015-08-31 21:32:52 +0200

    Progress Bar basic Version with _BackgroundWorker

---
+++ ProgressBarWPF/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ProgressBarWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            ViewModels.MainViewModel vm = new ViewModels.MainViewModel();
+            vm.TaskStarting += TaskStarted;
+            vm.ProgressChanged += ProgressChanged;
+            vm.TaskCompleted += TaskCompleted;
+
+            DataContext = vm;
+        }
+
+        void TaskStarted(object sender, EventArgs e)
+        {
+            this.Dispatcher.Invoke(new Action(() => ProgressPopup.IsOpen = true));
+        }
+
+        void TaskCompleted(object sender, System.ComponentModel.RunWorkerCompletedEventArgs e)
+        {
+            this.Dispatcher.Invoke(new Action(() => ProgressPopup.IsOpen = false));
+        }
+
+        void ProgressChanged(object sender, System.ComponentModel.ProgressChangedEventArgs e)
+        {
+            this.Dispatcher.Invoke(new Action(() => ProgressBar.Value = e.ProgressPercentage));
+        }
+    }
+}
---
+++ ProgressBarWPF/ViewModels/MainViewModel.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using System.ComponentModel;
+using System.Windows;
+using Microsoft.Practices.EnterpriseLibrary.Configuration.Design.ViewModel;
+using System.Threading;
+
+namespace ProgressBarWPF.ViewModels
+{
+
+    /// <summary>
+    /// Install-Package EnterpriseLibrary.Data
+    /// </summary>
+    public class MainViewModel : ViewModel, INotifyPropertyChanged
+    {
+        private BackgroundWorker _backgroundWorker;
+
+        #region Events
+
+        public event EventHandler TaskStarting = (s, e) => { };
+
+        public event ProgressChangedEventHandler ProgressChanged
+        {
+            add { _backgroundWorker.ProgressChanged += value; }
+            remove { _backgroundWorker.ProgressChanged -= value; }
+        }
+
+        public event RunWorkerCompletedEventHandler TaskCompleted
+        {
+            add { _backgroundWorker.RunWorkerCompleted += value; }
+            remove { _backgroundWorker.RunWorkerCompleted -= value; }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void RaisePropertyChangedEvent(string propertyName)
+        {
+            var handler = PropertyChanged;
+            if (handler != null)
+                handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion
+
+        #region Properties
+
+        private ICommand _executeImportation;
+        public ICommand ExecuteImportation
+        {
+            get
+            {
+                if (_executeImportation == null)
+                {
+                    _executeImportation = new DelegateCommand(param => _backgroundWorker.RunWorkerAsync());
+                }
+                return _executeImportation;
+            }
+        }
+
+        #endregion
+
+        public MainViewModel()
+        {
+            _backgroundWorker = new BackgroundWorker();
+            _backgroundWorker.WorkerReportsProgress = true;
+            _backgroundWorker.DoWork += executeTask;
+        }
+
+        private void executeTask(object sender, DoWorkEventArgs e)
+        {
+            OnTaskStarting();
+
+            for (int i = 0; i < 100; i++)
+            {
+                Thread.Sleep(100);
+                _backgroundWorker.ReportProgress(i + 1);                
+            }
+        }
+
+        private void OnTaskStarting()
+        {
+            TaskStarting(this, EventArgs.Empty);
+        }
+
+
+    }
+}
